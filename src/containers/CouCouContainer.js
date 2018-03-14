57abbc9ce65295a7878f61749c888b6f287f9b16
github username <github email address>
2018-03-14 17:55:37 -0400

    first commit

---
+++ src/containers/CouCouContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class CouCouContainer extends React.Component {
+  // state = {
+  //
+  // }
+  render() {
+    return <div> Welcome to CouCou! </div>;
+  }
+}
+
+export default CouCouContainer;
