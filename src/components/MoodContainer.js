05b7e09e4e7039de02048f91debcf39d338af5e9
github username <github email address>
2018-07-12 18:02:14 -0700

    added hovering text over mood images

--- src/components/MoodContainer.js
+++ src/components/MoodContainer.js
@@ -23,54 +23,70 @@ class MoodContainer extends React.Component {
                 id="1"
               />
               <div className="hvrbox-layer_top">
-                <div className="hvrbox-text">really sad</div>
+                <div className="hvrbox-text">let me cry</div>
               </div>
             </Button>
           </div>
-
-          <Button className="clickable-images" bsStyle="sucess">
-            <img
-              height="50"
-              width="50"
-              src="https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/118/disappointed-face_1f61e.png"
-              onClick={this.props.setMood}
-              alt="image2"
-              id="2"
-            />
-          </Button>
-
-          <Button className="clickable-images" bsStyle="sucess">
-            <img
-              height="50"
-              width="50"
-              src="https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/118/neutral-face_1f610.png"
-              onClick={this.props.setMood}
-              alt="image3"
-              id="3"
-            />
-          </Button>
-
-          <Button className="clickable-images" bsStyle="sucess">
-            <img
-              height="50"
-              width="50"
-              src="https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/118/grinning-face_1f600.png"
-              onClick={this.props.setMood}
-              alt="image4"
-              id="4"
-            />
-          </Button>
-
-          <Button className="clickable-images" bsStyle="sucess">
-            <img
-              height="50"
-              width="50"
-              src="https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/118/upside-down-face_1f643.png"
-              onClick={this.props.setMood}
-              alt="image5"
-              id="5"
-            />
-          </Button>
+          <div className="hvrbox">
+            <Button className="clickable-images" bsStyle="sucess">
+              <img
+                height="50"
+                width="50"
+                src="https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/118/disappointed-face_1f61e.png"
+                onClick={this.props.setMood}
+                alt="image2"
+                id="2"
+              />
+              <div className="hvrbox-layer_top">
+                <div className="hvrbox-text">not the best</div>
+              </div>
+            </Button>
+          </div>
+          <div className="hvrbox">
+            <Button className="clickable-images" bsStyle="sucess">
+              <img
+                height="50"
+                width="50"
+                src="https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/118/neutral-face_1f610.png"
+                onClick={this.props.setMood}
+                alt="image3"
+                id="3"
+              />
+              <div className="hvrbox-layer_top">
+                <div className="hvrbox-text">the usual</div>
+              </div>
+            </Button>
+          </div>
+          <div className="hvrbox">
+            <Button className="clickable-images" bsStyle="sucess">
+              <img
+                height="50"
+                width="50"
+                src="https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/118/grinning-face_1f600.png"
+                onClick={this.props.setMood}
+                alt="image4"
+                id="4"
+              />
+              <div className="hvrbox-layer_top">
+                <div className="hvrbox-text">the best</div>
+              </div>
+            </Button>
+          </div>
+          <div className="hvrbox">
+            <Button className="clickable-images" bsStyle="sucess">
+              <img
+                height="50"
+                width="50"
+                src="https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/118/upside-down-face_1f643.png"
+                onClick={this.props.setMood}
+                alt="image5"
+                id="5"
+              />
+              <div className="hvrbox-layer_top">
+                <div className="hvrbox-text">OMG</div>
+              </div>
+            </Button>
+          </div>
         </div>
       </div>
     );
